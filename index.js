64262156ba77deeed04e1af4a519937c648cff3a
John Smith <vinghiem97@gmail.com>
2023-02-14 15:46:34 +0700

    fix index

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const express = require('express')
 var Userrouter = require('./Router/AccontRouter/User')
-// const bodyParser = require('body-parser')
-// const urlNew = bodyParser.urlencoded({extended:false})
-
 const route = require('./Router/indexroutes')
-
 var morgan = require('morgan')
 const app = express()
-const { connect,mssql } = require('./Database/configSQL')
+
 app.use(morgan('combined'))
 app.use(express.urlencoded())
 app.use(express.json())
 
 
-
 route(app)
-
-// app.use('/user',Userrouter)
-
 
 app.get('/',async (req, res) => {
  res.send("sdadasdsadsada")
@@ -27,4 +19,4 @@ app.get('/',async (req, res) => {
 
 
 
-app.listen(3020)
+app.listen(process.env.PORT)
